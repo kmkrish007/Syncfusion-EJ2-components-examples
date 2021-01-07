e65ed27251126c7bf5560ecee7df53efc8389255
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-01-07 12:03:25 +0530

    Hosted application blazor

---
+++ FileManager/Blazor/Hosted Application Local service (Getting started)/FileManager/Server/Controllers/SampleDataController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.EJ2.FileManager.Base;
+using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace FileManager.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller
+    {
+        public PhysicalFileProvider operation;
+        public string basePath;
+
+        [Obsolete]
+        public SampleDataController(IHostingEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            this.operation = new PhysicalFileProvider();
+            this.operation.RootFolder(this.basePath + "\\Data\\Files"); // Data\\Files denotes in which files and folders are available.
+        }
+
+        // Processing the File Manager operations
+        [Route("FileOperations")]
+        public object FileOperations([FromBody] FileManagerDirectoryContent args)
+        {
+            switch (args.Action)
+            {
+                // Add your custom action here
+                case "read":
+                    // Path - Current path; ShowHiddenItems - Boolean value to show/hide hidden items
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                case "delete":
+                    // Path - Current path where of the folder to be deleted; Names - Name of the files to be deleted
+                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names));
+                case "copy":
+                    //  Path - Path from where the file was copied; TargetPath - Path where the file/folder is to be copied; RenameFiles - Files with same name in the copied location that is confirmed for renaming; TargetData - Data of the copied file
+                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "move":
+                    // Path - Path from where the file was cut; TargetPath - Path where the file/folder is to be moved; RenameFiles - Files with same name in the moved location that is confirmed for renaming; TargetData - Data of the moved file
+                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "details":
+                    // Path - Current path where details of file/folder is requested; Name - Names of the requested folders
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names));
+                case "create":
+                    // Path - Current path where the folder is to be created; Name - Name of the new folder
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name));
+                case "search":
+                    // Path - Current path where the search is performed; SearchString - String typed in the searchbox; CaseSensitive - Boolean value which specifies whether the search must be casesensitive
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                case "rename":
+                    // Path - Current path of the renamed file; Name - Old file name; NewName - New file name
+                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName));
+            }
+            return null;
+        }
+
+        // Processing the Download operation
+        [Route("Download")]
+        public IActionResult Download(string downloadInput)
+        {
+            //Invoking download operation with the required paramaters
+            // path - Current path where the file is downloaded; Names - Files to be downloaded;
+            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            return operation.Download(args.Path, args.Names);
+        }
+
+        // Processing the Upload operation
+        [Route("Upload")]
+        public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action)
+        {
+            //Invoking upload operation with the required paramaters
+            // path - Current path where the file is to uploaded; uploadFiles - Files to be uploaded; action - name of the operation(upload)
+            FileManagerResponse uploadResponse;
+            uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+        }
+
+        // Processing the GetImage operation
+        [Route("GetImage")]
+        public IActionResult GetImage(FileManagerDirectoryContent args)
+        {
+            //Invoking GetImage operation with the required paramaters
+            // path - Current path of the image file; Id - Image file id;
+            return this.operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+    }
+}
