fdcc0318bfd041fd3a7d3ba9756e2b7284ef93d7
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-04-20 12:49:58 +0530

    Prevent node drga stop event.

--- TreeView/Blazor/Prevent NodeDropped/BlazorApp1/Startup.cs
+++ TreeView/Blazor/Prevent NodeDropped/BlazorApp1/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Syncfusion.Blazor;
 
 namespace BlazorApp1
 {
@@ -29,6 +30,7 @@ namespace BlazorApp1
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSyncfusionBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
